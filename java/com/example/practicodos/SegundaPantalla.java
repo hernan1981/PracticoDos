5b9f9f96932b8ecdd53ec52b5a95d34beaaa6ec1
Hernan Rosas <hernanrs1981@gmail.com>
2020-06-16 22:25:16 -0300

    Primera version actividad dos

---
+++ java/com/example/practicodos/SegundaPantalla.java
@@ -0,0 +1,14 @@
+package com.example.practicodos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundaPantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_pantalla);
+    }
+}
